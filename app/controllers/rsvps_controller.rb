547378fbcdf7d20d5571c9077b0250fb99f64589
markeban <markeban@yahoo.com>
2015-06-16 21:05:11 -0500

    filled in show and index action, created custom method 'full_name'

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -1,6 +1,23 @@
 class RsvpsController < ApplicationController
 
+  def index
+    @rsvps = Rsvp.all
+  end
+
   def new
     @rsvp = Rsvp.new
   end
+
+  def show
+    @rsvp = Rsvp.find_by(id: params[:id])
+  end
+
+  def create
+    Rsvp.create(params[:rsvp])
+    redirect_to "/rsvps"
+  end
+
+
+
+
 end
